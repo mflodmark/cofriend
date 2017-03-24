df48862d5efdd19e9032ea3eac9eff4d2e1cf43e
Markus Flodmark <Markus@markuss-air-2.lan>
2017-03-24 23:00:15 +0100

    Initial Commit

---
+++ cofriend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cofriend
+//
+//  Created by Markus Flodmark on 2017-03-24.
+//  Copyright © 2017 Markus Flodmark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
